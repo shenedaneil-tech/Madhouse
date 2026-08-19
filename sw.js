12eb23774bde45d891955b0db351c76b8dd4d151
shenedaneil-tech <shenedaneil@gmail.com>
2026-08-19 12:23:52 -0700

    Refresh cache for generated art UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='madhouse-v7-real-generated-art';
+const CACHE='madhouse-v8-generated-ui-enabled';
 const ASSETS=['./','./index.html','./styles.css','./generated-art.css','./app.js','./manifest.webmanifest','./house-bg.svg','./kitchen-bg.svg','./assets/house-game.webp','./assets/kitchen-game.webp'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
