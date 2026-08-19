d3300cb3f7f372a3fe2c4cff12bf7c54e324b6bc
shenedaneil-tech <shenedaneil@gmail.com>
2026-08-19 10:43:31 -0700

    Refresh PWA cache for high-resolution full-screen art

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='madhouse-v4-generated-art';
+const CACHE='madhouse-v5-hires-flush';
 const ASSETS=['./','./index.html','./styles.css','./generated-art.css','./app.js','./manifest.webmanifest','./house-bg.svg','./kitchen-bg.svg','./assets/house-game.b64','./assets/kitchen-game.b64'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
